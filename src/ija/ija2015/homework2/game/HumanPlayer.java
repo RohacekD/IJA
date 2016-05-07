d6dc4f038b2644c6be22bc04dfd9b048bd0f31e4
tremors0 <janpavlu1@gmail.com>
2016-05-07 02:20:42 +0200

    javadoc - Artificall Inteligence, Game, AiPlayer, HumanPlayer refactor  - Player (putDisk, canPutDisk)

--- src/ija/ija2015/homework2/game/HumanPlayer.java
+++ src/ija/ija2015/homework2/game/HumanPlayer.java
@@ -9,16 +9,31 @@ import java.io.Serializable;
  * @author xjelin42, xpavlu08
  */
 public class HumanPlayer extends Player implements Serializable {
-
+    
+    /**
+     * Konstruktor vytvoří hráče typu člověk.
+     * @param isWhite Barva hráče
+     */
     public HumanPlayer(boolean isWhite) {
         super(isWhite);
     }
-
+    
+    
+    /**
+     * Metoda vrací vždy false. Tuto metodu implemetuje AiPlayer.
+     * @param game  Hra, kterou hráč hraje.
+     * @return false
+     */
     @Override
     public boolean putDisk(Game game) {
         return false;
     }
-
+    
+    
+    /**
+     * Vrací typ inteligence hráče.
+     * @return typ inteligence
+     */
     @Override
     public Player.Ai getInteligence() {
         return human;
--- src/ija/ija2015/homework2/game/Player.java
+++ src/ija/ija2015/homework2/game/Player.java
@@ -20,9 +20,7 @@ public abstract class Player implements Serializable{
     protected Disk[] pool;
     protected int takenFromPool;
     protected int countOfDisk;
-    protected boolean initialized;
     protected final ArrayList<Disk> toTurnOver;
-    protected boolean alreadyChecked;
     protected Undo undo;
 
     /**
@@ -36,12 +34,11 @@ public abstract class Player implements Serializable{
         takenFromPool = 0;
         countOfDisk = 0;
         toTurnOver = new ArrayList<>();
-        initialized = false;
-        alreadyChecked = false;
         legals = new ArrayList<>();
         undo = new Undo();
     }
-
+    
+    
     /**
      * Test barvy hráče.
      *
@@ -50,7 +47,8 @@ public abstract class Player implements Serializable{
     public boolean isWhite() {
         return this.white;
     }
-
+    
+    
     /**
      * Test, zda je možné vložit nový kámen hráče na dané pole. Kámen se sady
      * nevybírá ani nevkládá na pole.
@@ -59,52 +57,54 @@ public abstract class Player implements Serializable{
      * @return Zda je možné kámen vložit.
      */
     public boolean canPutDisk(Field field) {
-        Field.Direction chosenWay;
-        Field tmp2;
-        boolean result = false;
-        int removeFromIndex = 0;
-        toTurnOver.clear();
-
-        if (initialized == false) {
-            if (field.isEmpty() == true) {
-                if (takenFromPool < countOfDisk) {
-                    return true;
+        Field.Direction chosenWay; //smer, v nemz se bude tahnout
+        Field fieldInDir; //policko ve smeru, jimz pokracuji
+        boolean result = false; //promenna, ve ktere bude vysledek funkce
+        int removeFromIndex = 0; //index v arraylistu, od nejz se vymazou disky
+        
+        toTurnOver.clear(); //na zacatku vymazi obsah arraylistu
+        
+        //mohu vkladat jen na prazdne policko
+        if (field.isEmpty()) {
+            //prochazim pres vsechny smery
+            for (Field.Direction dir : Field.Direction.values()) {
+                if (field.nextField(dir).isEmpty() || field.nextField(dir).getDisk() == null
+                    || field.nextField(dir).getDisk().isWhite() == this.white 
+                    || field.nextField(dir).getDisk().isFreeze()) {
+                    //nedelam nic - nelze pokracovat timto smerem
                 }
-            }
-        } else {
-            if (field.isEmpty()) {
-                for (Field.Direction dir : Field.Direction.values()) {
-                    if (field.nextField(dir).isEmpty() || field.nextField(dir).getDisk() == null
-                            || field.nextField(dir).getDisk().isWhite() == this.white ||
-                            field.nextField(dir).getDisk().isFreeze()) {
-                    } else {
-                        chosenWay = dir;
-                        boolean result_tmp = false;
-                        tmp2 = field.nextField(chosenWay);
-                        toTurnOver.add(tmp2.getDisk());
-
-                        while (!tmp2.nextField(chosenWay).isEmpty() &&
-                                tmp2.nextField(chosenWay).canPutDisk(null) != false &&
-                                !tmp2.nextField(chosenWay).getDisk().isFreeze()) {
-                            if (tmp2.nextField(chosenWay).getDisk().isWhite() == this.white) {
-                                result = true;
-                                removeFromIndex = toTurnOver.size();
-                                break;
-                            } else {
-                                tmp2 = tmp2.nextField(chosenWay);
-                                toTurnOver.add(tmp2.getDisk());
-                            }
+                else { /* nasel jsem smer, jimz mohu pokracovat */
+                    chosenWay = dir;
+                    fieldInDir = field.nextField(chosenWay);
+                    toTurnOver.add(fieldInDir.getDisk()); //pridame policko do seznamu, v nemz jsou disky na otoceni
+                    
+                    //pokracuji, dokud nenarazim na stenu, prazdne policko, nebo zmrzly disk
+                    while (!fieldInDir.nextField(chosenWay).isEmpty() &&
+                            fieldInDir.nextField(chosenWay).canPutDisk(null) != false &&
+                           !fieldInDir.nextField(chosenWay).getDisk().isFreeze()) {
+                        
+                        //narazil jsem na disk stejne barvy - mam kompletni sezanm disku k otoceni
+                        if (fieldInDir.nextField(chosenWay).getDisk().isWhite() == this.white) {
+                            result = true; //nasel jsem minimalne jednu sadu disku k otoceni -> uspech
+                            removeFromIndex = toTurnOver.size(); /* posunu index, od nejz by se mazaly disky 
+                                                                    pridane do seznamu otacenych disku */
+                            break;
                         }
-                        if (result_tmp == false) {
-                            toTurnOver.subList(removeFromIndex, toTurnOver.size()).clear();
+                        else { //disk je opacne barvy, pridam ho do seznamu disku k otoceni a pokracuji
+                            fieldInDir = fieldInDir.nextField(chosenWay);
+                            toTurnOver.add(fieldInDir.getDisk());
                         }
                     }
+                    /* pokud je to mozne, oriznu seznam
+                       situace, kdy jsem nasel smer, ve kterem jsou souperovy disky, ale uz jsem nenarazil na
+                       disk me barvy, ktery by je uzaviral*/
+                    toTurnOver.subList(removeFromIndex, toTurnOver.size()).clear();
                 }
             }
         }
-        alreadyChecked = true;
         return result;
     }
+    
     
     /**
      * Vratí ArrayList všech polí kam tento hráč může táhnout a nastaví aktuální
@@ -126,7 +126,8 @@ public abstract class Player implements Serializable{
         this.legals = legals;
         return legals;
     }
-
+    
+    
     /**
      * Test prázdnosti sady kamenů, které má hráč k dispozici.
      *
@@ -136,6 +137,7 @@ public abstract class Player implements Serializable{
         return this.pool == null;
     }
 
+    
     /**
      * Vloží nový kámen hráče na dané pole, pokud to pravidla umožňují. Pokud
      * lze vložit, vybere kámen ze sady a vloží na pole.
@@ -144,37 +146,28 @@ public abstract class Player implements Serializable{
      * @return Úspěch akce.
      */
     public boolean putDisk(Field field) {
-        if (alreadyChecked == true) {
-            if(takenFromPool>= countOfDisk) return false;
-            field.putDisk(pool[takenFromPool]);
-            System.out.println("pokladam: "+takenFromPool);
-            takenFromPool++;
-            if (toTurnOver != null && !toTurnOver.isEmpty()) {
-                for (Disk tmp : toTurnOver) {
-                    tmp.turn();
-                }
-                initUndo(field, toTurnOver);
-                toTurnOver.clear();
+        if(takenFromPool>= countOfDisk) return false;
+        field.putDisk(pool[takenFromPool]);
+        takenFromPool++;
+        if (toTurnOver != null && !toTurnOver.isEmpty()) {
+            for (Disk tmp : toTurnOver) {
+                tmp.turn();
             }
-            return true;
-        } else if (canPutDisk(field)) {
-            System.out.println("pokladam: "+takenFromPool);
-            if(takenFromPool>= countOfDisk) return false;
-            field.putDisk(pool[takenFromPool]);
-            takenFromPool++;
-            if (toTurnOver != null && !toTurnOver.isEmpty()) {
-                for (Disk tmp : toTurnOver) {
-                    tmp.turn();
-                }
-                toTurnOver.clear();
-            }
-            return true;
+            initUndo(field, toTurnOver);
+            toTurnOver.clear();
         }
-        return false;
+        return true;
     }
     
+    
+    /**
+     * Abstraktní třída, kterou implementuje AiPlayer - provádí tahy
+     * @param game  Aktuální hra
+     * @return vrací true, pokul lze vložit disk
+     */
     public abstract boolean putDisk(Game game);
-
+    
+    
     /**
      * Inicializace hráče v rámci hrací desky. Vytvoří sadu kamenů o příslušné
      * velikosti a umístí své počáteční kameny na desku.
@@ -182,13 +175,13 @@ public abstract class Player implements Serializable{
      * @param board Deska, v rámci které se hráč inicializuje.
      */
     public void init(Board board) {
-        int countOfDisks = board.getRules().numberDisks();
         int firstWhite = board.getSize() / 2;
         Field tmp;
-        this.countOfDisk = countOfDisks;
-        this.pool = new Disk[countOfDisks];
+        
+        this.countOfDisk = board.getRules().numberDisks();;
+        this.pool = new Disk[countOfDisk];
 
-        for (int i = 0; i < countOfDisks; i++) {
+        for (int i = 0; i < countOfDisk; i++) {
             if (white) {
                 this.pool[i] = new Disk(true);
             } else {
@@ -207,12 +200,20 @@ public abstract class Player implements Serializable{
             tmp = board.getField(firstWhite + 1, firstWhite);
             putDisk(tmp);
         }
-
-        initialized = true;
     }
     
+    
+    /**
+     * Vraci typ inteligence hráče.
+     * @return typ inteligence
+     */
     public abstract Ai getInteligence();
-        
+    
+    
+    /**
+     * Vrátí informaci o barvě hráče.
+     * @return string - barva hráče
+     */
     @Override
     public String toString() {
         String tmp = "";
@@ -222,19 +223,40 @@ public abstract class Player implements Serializable{
         return this.white == true ? "white|" + tmp  : "black|" + tmp; 
 
     }
-
+    
+    
+    /**
+     * Vrací počet disku vybraných z hráčovy zásoby.
+     * @return počet použitých disků
+     */
     public int getTakenFromPool() {
         return takenFromPool;
     }
-
+    
+    
+    /**
+     * Vrací seynam disků k otočení.
+     * @return seznam disků k otočení
+     */
     public ArrayList<Disk> getToTurnOver() {
         return toTurnOver;
     }
     
+    
+    /**
+     * Inicializuje objekt typu undo
+     * @param field Poslední políčko, na které byl vložen disk
+     * @param toTurn Seznam disků, které se otočily
+     */
     void initUndo(Field field, ArrayList<Disk> toTurn) {
         this.undo.addToUndoLists(field, toTurn);
     }
     
+    
+    /**
+     * Vrací desku do stavu před posledním tahem.
+     * @return true, pokud se podařilo tah vrátit
+     */
     public boolean undo() {
         if (this.undo.undo(takenFromPool - 1, this.white, this.pool)) {
             this.takenFromPool--;
@@ -242,12 +264,20 @@ public abstract class Player implements Serializable{
         }
         return false;
     }
-
+    
+    
+    /**
+     * getter pro Undo
+     * @return undo
+     */
     public Undo getUndo() {
         return undo;
     }
     
     
+    /**
+     * výčet typů inteligence 
+     */
     public static enum Ai {
 
         rand, minMax, human;
